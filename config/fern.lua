67b022c0afc5f74aa0159b376ae732f2a6d4b804
Kohta Takanami <ripplekouha@gmail.com>
2022-04-25 00:16:11 +0900

    [add] DS_Storeを削除

--- config/fern.lua
+++ config/fern.lua
@@ -2,6 +2,7 @@ local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<C-w>o', '<cmd>Fern . -reveal=% -drawer -toggle -width=40<CR>', opts)
 
 vim.g['fern#renderer'] = 'nerdfont'
+vim.g['fern#default_hidden'] = 1
 vim.cmd([[
   augroup my-glyph-palette
     autocmd! *
